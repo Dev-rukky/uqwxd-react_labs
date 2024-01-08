32d546a963697a0709dd9f9366107f68de4e92c8
Rukky <abrukevwe@gmail.com>
2024-01-08 17:47:00 +0100

    Done

--- react-redux-master/src/components/MyButton.js
+++ react-redux-master/src/components/MyButton.js
@@ -1,1 +1,13 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import increment from "../actions";
 
+
+const MyButton = () => {
+    const dispatch = useDispatch();
+    return (
+        <button onClick={() => dispatch(increment())}>Increment</button>
+    );
+}
+
+export default MyButton;
